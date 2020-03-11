64b5be982da0031e14c32cd072df9afc74031190
Emilio Bool <contact@emiliobool.com>
2020-03-10 21:45:27 -0700

    fix issues

--- amqp.js
+++ amqp.js
@@ -4,9 +4,9 @@ const { AMPQ_PREFETCH, AMQP_URL } = require('./constants')
 exports.conn = null
 exports.ch = null
 exports.connect = async ({ url, prefetch }) => {
-    const conn = exports.conn = await amqp.connect(amqpURL || AMQP_URL)
+    const conn = exports.conn = await amqp.connect(url || AMQP_URL)
     const ch = exports.ch = await conn.createChannel()
-    ch.prefetch(amqpPrefetch || AMPQ_PREFETCH)
+    ch.prefetch(prefetch || AMPQ_PREFETCH)
     return ch
 }
 
--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-
-module.exports = require('./ampqp')
+module.exports = require('./amqp')
