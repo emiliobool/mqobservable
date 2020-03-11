777f67b9f4ea65fc60ba6c32046cec040820a37a
Emilio Bool <contact@emiliobool.com>
2020-03-10 19:39:17 -0700

    rename to url/prefetch

--- amqp.js
+++ amqp.js
@@ -3,7 +3,7 @@ const amqp = require('amqplib')
 const { AMPQ_PREFETCH, AMQP_URL } = require('./constants')
 exports.conn = null
 exports.ch = null
-exports.connect = async ({ amqpURL, amqpPrefetch }) => {
+exports.connect = async ({ url, prefetch }) => {
     const conn = exports.conn = await amqp.connect(amqpURL || AMQP_URL)
     const ch = exports.ch = await conn.createChannel()
     ch.prefetch(amqpPrefetch || AMPQ_PREFETCH)
