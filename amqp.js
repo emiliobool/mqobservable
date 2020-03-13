6fdf29a339d8360740603fd8ae787b53444d415c
Emilio Bool <contact@emiliobool.com>
2020-03-12 18:43:16 -0700

    default consumer

--- amqp.js
+++ amqp.js
@@ -3,10 +3,10 @@ const amqp = require('amqplib')
 const { AMPQ_PREFETCH, AMQP_URL } = require('./constants')
 exports.conn = null
 exports.ch = null
-exports.connect = async ({ url, prefetch }) => {
-    const conn = exports.conn = await amqp.connect(url || AMQP_URL)
+exports.connect = async (options = {}) => {
+    const conn = exports.conn = await amqp.connect(options.url || AMQP_URL)
     const ch = exports.ch = await conn.createChannel()
-    ch.prefetch(prefetch || AMPQ_PREFETCH)
+    ch.prefetch(options.prefetch || AMPQ_PREFETCH)
     return ch
 }
 
--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ exports.AMPQ_PREFETCH = process.env.AMPQ_PREFETCH || 0
 exports.AMQP_URL = process.env.AMQP_URL
 exports.CONSUMER_EXCHANGE = process.env.CONSUMER_EXCHANGE || 'consumer_exchange'
 exports.PUBLISHER_EXCHANGE =
-    process.env.PUBLISHER_EXCHANGE || 'publisher_exchange'
+    process.env.PUBLISHER_EXCHANGE || 'consumer_exchange'
 exports.DEDUPE_EXCHANGE = process.env.DEDUPE_EXCHANGE || 'dedupe_exchange'
 exports.QUEUE_PREFIX = process.env.QUEUE_PREFIX || 'queue_'
 exports.REPLY_ROUTING_KEY = process.env.REPLY_ROUTING_KEY || uuidv1()
